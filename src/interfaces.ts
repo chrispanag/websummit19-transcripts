10feecda568574ebff2f79eb476f8d51a7e06f1d
Christos Panagiotakopoulos <chrispanag@gmail.com>
2019-11-08 17:50:32 +0200

    Refactoring

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,46 +1,59 @@
 export interface IGroupData {
-    status: string;
-    group: {
-        id: number;
-        name: string;
-        owner: any;
-        messages: ISpeechData[]
-    }
+  status: string;
+  group: {
+    id: number;
+    name: string;
+    owner: any;
+    messages: ISpeechData[];
+  };
+}
+
+export interface ISpeechDataDetails {
+  speech_id: string;
+  id: number;
+  start_time: number;
+  end_time: number;
+  modified_time: number;
+  is_deleted: boolean;
+  duration: number;
+  title: string;
+  summary: string;
+  appid: string;
+  total_words: number;
+  confidence: string;
+  process_finished: boolean;
+  is_read: boolean;
+  upload_finished: boolean;
+  hasPhotos: number;
+  download_url: string;
+  transcript_updated_at: number;
+  live_status: string;
+  live_status_message: string;
+  folder: any | null;
+  speakers: ISpeaker[];
+  transcripts: any[];
 }
 
 export interface ISpeechData {
-    id: string;
-    created_at: string;
-    last_modified_at: string;
-    group_id: number;
-    poster: any;
-    is_deleted: boolean;
-    speech: {
-        speech_id: string;
-        id: number;
-        start_time: number;
-        end_time: number;
-        modified_time: number;
-        is_deleted: boolean;
-        duration: number;
-        title: string;
-        summary: string;
-        appid: string;
-        total_words: number;
-        confidence: string;
-        process_finished: boolean;
-        is_read: boolean;
-        upload_finished: boolean;
-        hasPhotos: number;
-        download_url: string;
-        transcript_updated_at: number;
-        live_status: string;
-        live_status_message: string;
-        folder: any | null;
-    };
-    entire_speech: boolean;
-    snippets: any[];
-    note: any | null;
-    permission: string;
-    group: any | {};
+  id: string;
+  created_at: string;
+  last_modified_at: string;
+  group_id: number;
+  poster: any;
+  is_deleted: boolean;
+  speech: ISpeechDataDetails;
+  entire_speech: boolean;
+  snippets: any[];
+  note: any | null;
+  permission: string;
+  group: any | {};
+}
+
+export interface ISpeaker {
+  id: number;
+  created_at: string;
+  speaker_name: string;
+  url: string;
+  user_id: number;
+  self_speaker: boolean;
 }
--- src/preprocess.ts
+++ src/preprocess.ts
@@ -1,25 +1,45 @@
-import { promises as fs, read } from "fs";
+import { promises as fs } from "fs";
+import { ISpeaker, ISpeechDataDetails, ISpeechData } from "./interfaces";
 
 async function readData(path: string = "./plain-texts/data.json") {
   const data = await fs.readFile(path, "utf-8");
   const parsed = JSON.parse(data);
 
-  return parsed;
+  return parsed as ISpeechData[];
+}
+
+function extractSpeakerNames(speech: ISpeechDataDetails) {
+  const speakersRaw: ISpeaker[] = speech.speakers;
+  const speakers: { [key: string]: string } = {};
+  speakersRaw.forEach(speaker => (speakers[speaker.id] = speaker.speaker_name));
+
+  return speakers;
+}
+
+function createDialogue(
+  speakers: { [key: string]: string },
+  transcripts: any[]
+): string {
+  return transcripts.reduce(
+    (a: string, v: any) =>
+      a +
+      "\n" +
+      `${v.speaker_id ? speakers[v.speaker_id] : "Someone"}: ` +
+      v.transcript,
+    ""
+  );
 }
 
 (async () => {
   const data = await readData();
-  data.forEach((s: any) => {
-    const speakersRaw = s.speech.speakers;
-    const speakers: any = {};
-    speakersRaw.forEach((speaker: any) => speakers[speaker.id] = speaker.speaker_name);
-    
-    const transcripts = s.speech.transcripts as any[];
-    const string = transcripts.reduce((a: string, v: any) => a + "\n" + `${v.speaker_id ? speakers[v.speaker_id] : "Someone"}: ` + v.transcript, "");
+  data.forEach((s: ISpeechData) => {
+    const speakers: { [key: string]: string } = extractSpeakerNames(s.speech);
+    const speechTranscript = createDialogue(speakers, s.speech.transcripts);
+
     try {
-        fs.writeFile(`./plain-texts/${s.speech.speech_id}.txt`, string);
+      fs.writeFile(`./plain-texts/${s.speech.speech_id}.txt`, speechTranscript);
     } catch (err) {
-        throw err;
+      throw err;
     }
   });
 })();
